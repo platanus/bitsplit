263ebf1aaf1e974a249cdd5e01f0693c553e27d2
rihanuch <rihanuch@uc.cl>
2020-04-07 20:22:46 -0400

    add: user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resources :sessions, only: [:create, :destroy]
+    resource :sessions, only: [:create, :destroy]
+    resources :users, only: [:create]
   end
 end
