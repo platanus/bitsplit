d4b0be0d0e8e8b96ab50557ccbbc490e70d0d521
rihanuch <rihanuch@uc.cl>
2020-04-07 20:52:14 -0400

    add: valid methods for users and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resource :sessions, only: [:create, :destroy]
-    resources :users, only: [:create]
+    resource :users, only: [:create, :destroy]
   end
 end
