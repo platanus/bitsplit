b0d78ff80350fe3f506fad79c2450ef4a85116c5
Martin Anselmo <mfanselmo@uc.cl>
2020-04-09 18:34:17 -0400

    [web]feat: add signin, signup, landing and home components

---
+++ app/javascript/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Route: {{ routeName }}</h1>
+    <p>Current user: {{ currentUser }}</p>
+    <button @click="signOut">Log Out</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      routeName: 'Home',
+      signInRoute: 'sign-in',
+      signUpRoute: 'sign-up'
+    }
+  },
+
+  computed: {
+    ...mapState('user', ['currentUser'])
+  },
+  methods: {
+    ...mapActions('user', ['signOut']),
+    handleLogOut() {
+      this.logOut().then(() => {
+        this.$router.push('/')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
