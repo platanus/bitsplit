92b26f6a7ee69c15e5b8e9153f5ca2ad44a0a73f
Martin Anselmo <mfanselmo@uc.cl>
2020-04-09 18:36:07 -0400

    [web]feat: Add vue-router support

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,30 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div id="vue-app">
+    <div v-if="alert.message" :class="`alert ${alert.type}`">
+      {{ alert.message }}
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  name: 'app',
+  computed: {
+    ...mapState({
+      alert: state => state.alert
+    })
+  },
+  methods: {
+    ...mapActions('alert', ['clear_alert'])
+  },
+  watch: {
+    $route(to, from) {
+      // clear alert on location change
+      this.clear_alert()
     }
   }
 }
 </script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
