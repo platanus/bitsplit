687058c35745d0e16ed2eda398066dfa0038248e
Martin Anselmo <mfanselmo@uc.cl>
2020-04-09 18:34:17 -0400

    [web]feat: add signin, signup, landing and home components

---
+++ app/javascript/components/SignUp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ test }}</div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      test: 'hola from signUp'
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
