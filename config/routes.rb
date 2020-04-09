e80386dd90b0ebde3ef9b392102a659170eaff0d
Martin Anselmo <mfanselmo@uc.cl>
2020-04-09 18:36:07 -0400

    [web]feat: Add vue-router support

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   scope path: '/api' do
     api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
       resource :sessions, only: [:create, :destroy]
@@ -16,10 +15,6 @@ Rails.application.routes.draw do
 
   get '/*path', to: 'home#index'
 
-=======
-  root 'home#index'
-
->>>>>>> [web] feat: Add initial vue setup
   devise_for :users
 
 end
