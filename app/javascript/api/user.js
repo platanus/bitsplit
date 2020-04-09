1ec7ad4b859fc56458ad2ab1db0818ec769608a1
Martin Anselmo <mfanselmo@uc.cl>
2020-04-09 18:33:22 -0400

    [web]feat: Add class for API, initial mock API

---
+++ app/javascript/api/user.js
@@ -0,0 +1,26 @@
+import api from './api'
+
+const loginApi = payload => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          user: {
+            authentication_token: 'smnozAAbYnEX2xVRfS5R',
+            email: 'example@example.com',
+            api_key: 'th3b3stAP1K3Y'
+          }
+        }
+      })
+    }, 1000)
+  })
+}
+const logoutApi = payload => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({})
+    }, 1000)
+  })
+}
+
+export { loginApi, logoutApi }
