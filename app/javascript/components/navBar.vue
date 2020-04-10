b7d4188d43f3cdb0220d0e2df92e53221efac64b
Martin Anselmo <mfanselmo@uc.cl>
2020-04-10 18:27:48 -0400

    [web]fix: console.error
    Co-Authored-By: NachoCasta <icastanedaw@gmail.com>

--- app/javascript/components/navBar.vue
+++ app/javascript/components/navBar.vue
@@ -33,7 +33,7 @@ export default {
         })
         .catch(err => {
           // Aviso de que hubo un error se muestra globalmente
-          console.err(err)
+          console.error(err)
         })
     }
   },
@@ -43,4 +43,3 @@ export default {
   }
 }
 </script>
-
