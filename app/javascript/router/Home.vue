9255313205da6f6ca4d69cefad2d808b49c74957
Martin Anselmo <mfanselmo@uc.cl>
2020-04-10 11:35:27 -0400

    [web]Chore: Move components out of view

--- app/javascript/router/Home.vue
+++ app/javascript/router/Home.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Route: {{ routeName }}</h1>
     <p>Current user: {{ currentUser }}</p>
-    <button @click="signOut">Log Out</button>
   </div>
 </template>
 
@@ -13,22 +12,11 @@ export default {
   name: 'Home',
   data() {
     return {
-      routeName: 'Home',
-      signInRoute: 'sign-in',
-      signUpRoute: 'sign-up'
+      routeName: 'home'
     }
   },
-
   computed: {
     ...mapState('user', ['currentUser'])
-  },
-  methods: {
-    ...mapActions('user', ['signOut']),
-    handleLogOut() {
-      this.logOut().then(() => {
-        this.$router.push('/')
-      })
-    }
   }
 }
 </script>
