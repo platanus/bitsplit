4a4cc31060d5c4a8baccec35e1c34506fa3f0233
Martin Anselmo <mfanselmo@uc.cl>
2020-04-10 11:35:27 -0400

    [web]Chore: Move components out of view

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="vue-app">
-    <header></header>
-    <div v-if="alert.message" :class="`alert ${alert.type}`">{{ alert.message }}</div>
+    <navBar></navBar>
+    <alert-message></alert-message>
     <router-view></router-view>
   </div>
 </template>
@@ -9,17 +9,19 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 
+import navBar from './components/navBar.vue'
+import alertMessage from './components/alertMessage.vue'
+
 export default {
   name: 'app',
-  computed: {
-    ...mapState({
-      alert: state => state.alert
-    })
+
+  components: {
+    navBar,
+    alertMessage
   },
   methods: {
     ...mapActions('alert', ['clear_alert'])
   },
-
   watch: {
     $route(to, from) {
       // clear alert on location change
