9c933eda638f2724863b4e8b03de1d2d17d3cd60
Martin Anselmo <mfanselmo@uc.cl>
2020-04-10 21:48:10 -0400

    [web]fix: no landing for signed in user

--- app/javascript/router/Landing.vue
+++ app/javascript/router/Landing.vue
@@ -5,11 +5,21 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'Landing',
   data() {
     return {
       routeName: 'Landing'
+    }
+  },
+  computed: {
+    ...mapGetters('user', ['signedIn'])
+  },
+  created() {
+    if (this.signedIn) {
+      this.$router.push('/home')
     }
   }
 }
