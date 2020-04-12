a4729bbe6d0b0e9c76a10b370971cf348b91d8d1
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-12 17:11:43 -0400

    [web] delete: duplicated app spec 2

--- app/javascript/app.spec 2.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import app from 'app'
-
-describe('app', () => {
-  test('is a Vue instance', () => {
-    const wrapper = shallowMount(app)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
