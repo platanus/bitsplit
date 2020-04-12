ba02b7e2dce084c373e583b65be24b2b45f60c63
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-12 19:08:21 -0400

    [web] add: alertMessage

---
+++ app/javascript/components/alertMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="alert.message" :class="`alert ${alert.type}`">{{ alert.message }}</div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapState } from 'vuex'
+export default {
+  name: 'alertMessage',
+  computed: {
+    ...mapState({
+      alert: state => state.alert
+    })
+  }
+}
+</script>
