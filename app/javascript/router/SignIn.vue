59f110f1d9d9fb22e3812699f1330e44d0c50f0a
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-12 18:57:38 -0400

    [web] add: UI

--- app/javascript/router/SignIn.vue
+++ app/javascript/router/SignIn.vue
@@ -1,25 +1,58 @@
-<template>
-  <div>
-    <p>Route: {{ routeName }}</p>
-    <form @submit.prevent="handleSubmit">
-      <div>
-        <label for="email">email</label>
-        <input type="text" v-model="email" name="email" />
+<template id="full">
+  <div id="SignInForm">
+    <center>
+      <div class="w-full max-w-xs">
+        <form
+          @submit.prevent="handleSubmit"
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="email"
+            >
+              Email
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Email"
+              v-model="email"
+              name="email"
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              v-model="password"
+              name="password"
+              placeholder="******************"
+            />
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              :disabled="loading"
+            >
+              Log In
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" v-model="password" name="password" />
-      </div>
-      <div>
-        <button :disabled="loading">
-          Login
-        </button>
-      </div>
-    </form>
+    </center>
   </div>
 </template>
 
 <script>
+
+
 import { mapActions, mapState } from 'vuex'
 export default {
   name: 'SignIn',
@@ -53,4 +86,9 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style scoped>
+#SignInForm {
+  margin-top: 60px;
+  margin-bottom: 60px;
+}
+</style>
