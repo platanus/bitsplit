8d6400eed26c07b90befadd116de1b07423d3159
josefinabengoa <30700227+josefinabengoa@users.noreply.github.com>
2020-04-13 17:18:11 -0400

    [backend] Update quotations_controller
    Co-Authored-By: NachoCasta <icastanedaw@gmail.com>

--- app/controllers/api/v1/quotations_controller.rb
+++ app/controllers/api/v1/quotations_controller.rb
@@ -2,8 +2,10 @@ class Api::V1::QuotationsController < ApplicationController
    
     def create 
         market_id = 'BTC-CLP'
-        quotation_agv = make_buda_quotation_request(market_id, 'ask_given_value', params[:amount])
-        quotation_bgs = make_buda_quotation_request(market_id, 'bid_given_size', quotation_agv['quotation']['base_exchanged'][0])
+        final_amount = params[:amount]
+        quotation_agv = make_buda_quotation_request(market_id, 'ask_given_value', final_amount)
+        btc_amount = quotation_agv['quotation']['base_exchanged'][0]
+        quotation_bgs = make_buda_quotation_request(market_id, 'bid_given_size', btc_amount)
         return set_success_params(quotation_bgs) if quotation_bgs.has_key? "quotation"
         @error_message = quotation_bgs 
         render "error"
