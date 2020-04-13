530bb7636f7d9b36647c63ad24679a4cc2db1854
Martin Anselmo <mfanselmo@uc.cl>
2020-04-13 06:48:38 -0400

    [web]add: style and logic for alert system

---
+++ app/javascript/components/alerts/alertMessage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-on:click="CLEAR_ALERT()">
+    <errorAlert v-if="alert.type==='error'"></errorAlert>
+    <successAlert v-if="alert.type==='success'"></successAlert>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapState, mapMutations } from 'vuex'
+
+import errorAlert from './errorAlert'
+import successAlert from './successAlert'
+
+export default {
+  name: 'alertMessage',
+  components: {
+    errorAlert,
+    successAlert
+  },
+  methods: {
+    ...mapMutations('alert', ['CLEAR_ALERT'])
+  },
+  computed: {
+    ...mapState({
+      alert: state => state.alert
+    })
+  }
+}
+</script>
