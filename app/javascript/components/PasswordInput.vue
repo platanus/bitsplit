ef654b292399c057ffd255a5d7e13c3d573fd1fd
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-13 10:39:27 -0400

    [web] add: components, input button

---
+++ app/javascript/components/PasswordInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+    :id="fieldId"
+    type="password"
+    :placeholder="fieldPlaceholder"
+    :name="fieldName"
+    :value="value"
+    @input="$emit('update', $event.target.value)"
+  />
+</template>
+<script>
+export default {
+  name: 'PasswordForm',
+  props: {
+    value: String,
+    fieldId: String,
+    fieldPlaceholder: String,
+    fieldName: String
+  },
+  model: {
+    prop: 'value',
+    event: 'update'
+  }
+}
+</script>
