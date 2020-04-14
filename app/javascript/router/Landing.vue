38a6a0c95ff84caacbcda4c4a4ee3aa445927268
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-14 16:53:36 -0400

    [web] fix: Langing

--- app/javascript/router/Landing.vue
+++ app/javascript/router/Landing.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
     <p>Route: {{ routeName }}</p>
-
-    <router-link :to="signInRoute">sign in</router-link>
-    <router-link :to="signUpRoute">sign up</router-link>
-    <router-link :to="homeRoute">Home</router-link>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 export default {
   name: 'Landing',
   data() {
     return {
-      routeName: 'Landing',
-      signInRoute: 'sign-in',
-      signUpRoute: 'sign-up',
-      homeRoute: 'home'
+      routeName: 'Landing'
+    }
+  },
+  computed: {
+    ...mapGetters('user', ['signedIn'])
+  },
+  created() {
+    if (this.signedIn) {
+      this.$router.push('/home')
     }
   }
 }
