c1af4bc227a06da787285083aa2d5d82ba88ffdd
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-14 19:17:58 -0400

    [web] del: prop

--- app/javascript/store/modules/user/state.js
+++ app/javascript/store/modules/user/state.js
@@ -2,9 +2,5 @@ const user = JSON.parse(localStorage.getItem('currentUser'))
 
 export default {
   currentUser: user, // Si no esta es null
-  userLoading: false,
-  token: null,
-  buda: '',
-  api_key: '',
-  authToken: ''
+  userLoading: false
 }
