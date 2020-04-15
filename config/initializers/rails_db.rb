ede695315dc8288a0eb8b3a4ace35cd704d4a933
Daniel Darritchon <dedarritchon@uc.cl>
2020-04-15 18:28:53 -0400

    [backend] del: quito basic_auth de rails_db (as default)

--- config/initializers/rails_db.rb
+++ config/initializers/rails_db.rb
@@ -13,13 +13,13 @@ if Object.const_defined?('RailsDb')
     # config.white_list_tables = ['posts', 'comments']
 
     # # Enable http basic authentication
-    config.http_basic_authentication_enabled = false
+    # config.http_basic_authentication_enabled = true
 
     # # Enable http basic authentication
-    config.http_basic_authentication_user_name = 'rails_db'
+    # config.http_basic_authentication_user_name = 'rails_db'
 
     # # Enable http basic authentication
-    config.http_basic_authentication_password = 'password'
+    # config.http_basic_authentication_password = 'password'
 
     # # Enable http basic authentication
     # config.verify_access_proc = proc { |controller| true }
