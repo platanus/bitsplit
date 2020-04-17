2432ea21afe24a432b27d4ab183b300d0c92702e
Ignacio Castañeda <icastanedaw@gmail.com>
2020-04-17 18:48:44 -0400

    [backend] fix: auto generated tests

--- spec/helpers/api/v1/quotations_helper_spec.rb
+++ spec/helpers/api/v1/quotations_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the Api::V1::Transaction::PaymentHelper. For example:
+# the Api::V1::QuotationsHelper. For example:
 #
-# describe Api::V1::Transaction::PaymentHelper do
+# describe Api::V1::QuotationsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe Api::V1::Transaction::PaymentHelper, type: :helper do
+RSpec.describe Api::V1::QuotationsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
