c181b014cd8fdd104d8d617dad036afdd0b668d1
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-04-20 16:01:22 -0400

    [web] add: payment path

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -6,6 +6,7 @@ import SignUp from './SignUp.vue'
 import Landing from './Landing.vue'
 import Home from './Home.vue'
 import BudaProfile from './BudaProfile.vue'
+import Payment from './Payment.vue'
 
 import { checkAuth, checkNoAuth } from '../helpers'
 
@@ -18,7 +19,8 @@ const router = new Router({
     { path: '/sign-in', component: SignIn, beforeEnter: checkNoAuth },
     { path: '/sign-up', component: SignUp, beforeEnter: checkNoAuth },
     { path: '/home', component: Home, beforeEnter: checkAuth },
-    { path: '/buda', component: BudaProfile, beforeEnter: checkAuth }
+    { path: '/buda', component: BudaProfile, beforeEnter: checkAuth },
+    { path: '/payment', component: Payment , beforeEnter: checkAuth }
   ]
 })
 
