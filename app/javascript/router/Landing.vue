8f73c0cdc6066ab6fabc2ffff2e2b8caf97286df
Martin Anselmo <mfanselmo@uc.cl>
2020-04-21 18:56:52 -0400

    [web]fix: Change double redirect to home on login

--- app/javascript/router/Landing.vue
+++ app/javascript/router/Landing.vue
@@ -15,11 +15,6 @@ export default {
   },
   computed: {
     ...mapGetters('user', ['signedIn'])
-  },
-  created() {
-    if (this.signedIn) {
-      this.$router.push('/home')
-    }
   }
 }
 </script>
