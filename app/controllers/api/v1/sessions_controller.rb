bebe8ac053645ec65ca7d8a65774e231f26a1f7a
Daniel Darritchon <dedarritchon@uc.cl>
2020-04-22 12:50:01 -0400

    [backend] add: firebase service

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,6 +7,13 @@ class Api::V1::SessionsController < Api::V1::BaseController
         @user = User.where(email: params[:email]).first
 
         if @user&.valid_password?(params[:password])
+
+            # prueba 
+            firebase = FirebaseService.new
+            firebase.save_token(@user)
+    
+            # prueba
+
             @user.update_attribute(:logged, true)
             @password = params[:password]
             render :create, status: :created
@@ -17,6 +24,7 @@ class Api::V1::SessionsController < Api::V1::BaseController
 
     def destroy
         current_user&.authentication_token = nil
+        puts "CHAOOO"
         if current_user.save
             @current_user.update_attribute(:logged, false)
             head(:no_content)
---
+++ app/services/firebase_service.rb
@@ -0,0 +1,38 @@
+class FirebaseService < PowerTypes::Service.new
+    # para llamarlo desde un controlador:
+    # firebase = FirebaseService.new
+    # firebase.buda_keys_update_notification(current_user)
+
+    def payment_notification(user, payment)
+        #get payment data and send notification
+        send_notification(user, "payment", "payment data example")
+    end
+
+    def login_notification(user)
+        #other notification wrapper
+        send_notification(user, "login", "user logged in")
+    end
+
+    def buda_keys_update_notification(user)
+        #get payment data and send notification
+        send_notification(user, "update", "user updated buda api keys")
+    end
+  
+    def send_notification(user, type, data)
+        key = user.email.split("@").first
+        response = @firebase.push("notifications/#{key}", {:type => type, :data => data, :seen => false})
+    end
+  
+    def save_token(user)
+        key = user.email.split("@").first
+        response = @firebase.update("tokens", { key.to_sym => user.authentication_token })
+    end
+  
+    private
+    def initialize
+        firebase_url = 'https://platanus-bitsplit.firebaseio.com/'
+        private_key_json_string = Rails.application.secrets.firebase_credentials
+        @firebase = Firebase::Client.new(firebase_url, private_key_json_string)
+    end
+  
+end
