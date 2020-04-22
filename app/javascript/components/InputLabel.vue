cb4f2c9f37aa7de375f4a9f3dab533c36cd054fd
Ignacio Castañeda <icastanedaw@gmail.com>
2020-04-22 14:13:20 -0400

    [web] fix: pass fieldFor prop correctly

--- app/javascript/components/InputLabel.vue
+++ app/javascript/components/InputLabel.vue
@@ -1,13 +1,11 @@
 <template>
-  <label class="block text-gray-700 text-sm font-bold mb-2" for= fieldFor >
-    {{ fieldName }}
-  </label>
+  <label class="block text-gray-700 text-sm font-bold mb-2" :for="fieldFor">{{ fieldName }}</label>
 </template>
 <script>
 export default {
   name: 'inputLabel',
   props: {
-    fieldName: String, 
+    fieldName: String,
     fieldFor: String
   }
 }
