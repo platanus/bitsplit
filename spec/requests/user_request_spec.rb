4ecd9c8f157a9b202f464d8577db828211cc3b4b
Ignacio Castañeda <icastanedaw@gmail.com>
2020-04-22 17:39:51 -0400

    [backend] tests: refactored user CRUD tests

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,90 +1,122 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+
+
+
+
+RSpec.describe User, type: :request do
   
   user_params = {"email" => "user@example.com", "password" =>"password", "password_confitmation" => "password", "api_secret" => "api_secret", "api_key" => "api_key"}
 
-  describe "Expected responses (all correct)" do
+  describe "CRUD" do
 
-    it "POST: create user and return its corresponding data" do
-      response = create_user(user_params)
-      expect(response).to have_http_status(:created)
-      response_body = get_response_body(response)
-      expect(response_body["data"]["type"]).to eq "user"
-
-      # test if data is still the same
-      expect(response_body["data"]["attributes"]).to include("email" => "user@example.com", "api_key" => "api_key")
-      # must return auth_token
-      expect(response_body["data"]["attributes"]).to have_key("authentication_token")
-      
-      # do not include password or api_secret
-      expect(response_body["data"]["attributes"]).not_to (have_key("password") && have_key("api_secret"))
+    before(:all) do
+      User.delete_all
+      @post_response = create_user(user_params)
+      @post_response_body = get_response_body(@post_response)
     end
 
-    it "GET: current user" do
-      response = create_user(user_params)
-      response_body = get_response_body(response)
-      auth_token = get_by_attribute(response_body, "authentication_token")
-
-      get "/api/v1/users", headers: create_headers(user_params["email"], auth_token)
-
-      # test if data is still the same
-      expect(response_body["data"]["attributes"]).to include("email" => "user@example.com", "api_key" => "api_key")
-      # must return auth_token
-      expect(response_body["data"]["attributes"]).to have_key("authentication_token")
-      
-      # do not include password or api_secret
-      expect(response_body["data"]["attributes"]).not_to (have_key("password") && have_key("api_secret"))
+    before(:each) do
+      auth_token = get_by_attribute(@post_response_body, "authentication_token")
+      @headers = create_headers(user_params["email"], auth_token)
     end
 
-    it "PATCH: current user" do
-      _response = create_user(user_params)
-      _response_body = get_response_body(_response)
-      auth_token = get_by_attribute(_response_body, "authentication_token")
+    describe "POST: create user and return its corresponding data" do
+      it "should have created status" do 
+        expect(@post_response).to have_http_status(:created)
+      end
+      it "should have user type" do
+        expect(@post_response_body["data"]["type"]).to eq "user"
+      end
 
-      patch "/api/v1/users", headers: create_headers(user_params["email"], auth_token), params: {"email" => "newuser@example.com", "api_key": "new_api_key", "password" => user_params["password"]}
-
-      response_body = get_response_body(response)
-
-      # test if data is modified the same
-      expect(response_body["data"]["attributes"]).to include("email" => "newuser@example.com", "api_key" => "new_api_key")
-      # must return auth_token
-      expect(response_body["data"]["attributes"]).to have_key("authentication_token")
+      it "should still be the same data" do
+        expect(@post_response_body["data"]["attributes"]).to include("email" => user_params["email"], "api_key" => user_params["api_key"])
+      end
+      it "should return auth_token" do
+        expect(@post_response_body["data"]["attributes"]).to have_key("authentication_token")
+      end
       
-      # do not include password or api_secret
-      expect(response_body["data"]["attributes"]).not_to (have_key("password") && have_key("api_secret"))
-
-      # cannot update as it does not have the password (or a password)
-      patch "/api/v1/users", headers: create_headers("newuser@example.com", auth_token), params: {"api_secret": "new_api_secret"}
-      expect(response).to have_http_status(:bad_request)
+      it "should not include password or api_secret" do
+        expect(@post_response_body["data"]["attributes"]).not_to (have_key("password") && have_key("api_secret"))
+      end
     end
 
-    it "DELETE: current user" do
-      _response = create_user(user_params)
-      response_body = get_response_body(_response)
-      auth_token = get_by_attribute(response_body, "authentication_token")
+    describe "GET: current user" do
+      before(:each) do
+        get "/api/v1/users", headers: @headers
+        @response_body = get_response_body(response)
+      end
+      it "should still be the same data" do
+        expect(@response_body["data"]["attributes"]).to include("email" => user_params["email"], "api_key" => user_params["api_key"])
+      end
+      it "should return auth token" do
+        expect(@response_body["data"]["attributes"]).to have_key("authentication_token")
+      end
+      it "should not include password or api_secret" do
+        expect(@response_body["data"]["attributes"]).not_to (have_key("password") && have_key("api_secret"))
+      end
+    end
 
-      delete "/api/v1/users", headers: create_headers(user_params["email"], auth_token), params: {"email" => user_params["email"], "password" => user_params["password"]}
-      expect(response).to have_http_status(:no_content)
+    describe "PATCH: current user" do
+      new_email = "newuser@example.com"
+      new_api_key = "new_api_key"
+
+      context "with password" do
+        before(:each) do
+          patch "/api/v1/users", headers: @headers, params: {"email" => new_email, "api_key": new_api_key, "password" => user_params["password"]}
+          @response_body = get_response_body(response)
+        end
+        it "should have new data" do
+          expect(@response_body["data"]["attributes"]).to include("email" => new_email, "api_key" => new_api_key)
+        end
+        it "should return auth token" do
+          expect(@response_body["data"]["attributes"]).to have_key("authentication_token")
+        end
+        it "should not include password or api_secret" do
+          expect(@response_body["data"]["attributes"]).not_to (have_key("password") && have_key("api_secret"))
+        end
+      end
+      
+      context "without password" do
+        before(:each) do
+          patch "/api/v1/users", headers: @headers, params: {"email" => new_email, "api_key": new_api_key}
+          @response = response
+        end
+        it "should get bad request" do
+          expect(@response).to have_http_status(:bad_request)
+        end
+      end
+    end
+
+    describe "DELETE: current user" do
+      before(:each) do
+        delete "/api/v1/users", headers: @headers, params: {"email" => user_params["email"], "password" => user_params["password"]}
+        @reponse = response
+      end
+      it "should be deleted" do
+        expect(@response).to have_http_status(:success)
+      end
     end 
 
-  end
+    after(:all) do
+      User.delete_all
+    end
 
-  private
+  end
 
   def get_by_attribute(json, param)
     json["data"]["attributes"][param]
   end
-
+  
   def create_user(user_params)
     post  "/api/v1/users", params: user_params
     return response
   end
-
+  
   def get_response_body(response)
     return JSON.parse(response.body)
   end
-
+  
   def create_headers(email, auth_token)
     { "X-User-Email" => email, "X-User-Token": auth_token }
   end
