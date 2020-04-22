fb9b88241a82a373b5d855c626813bd24516331c
Martin Anselmo <mfanselmo@uc.cl>
2020-04-22 15:39:51 -0400

    [web]feat: Button and profile components

---
+++ app/javascript/components/LinkButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    @click="moveMe()"
+    :class="[classmod,'text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline']"
+    :disabled="fieldDisabled"
+  >{{ fieldPlaceholder }}</button>
+</template>
+<script>
+export default {
+  name: 'LinkButton',
+  props: {
+    fieldDisabled: Boolean,
+    fieldPlaceholder: String,
+    route: String,
+    classmod: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    moveMe() {
+      this.$router.push(`/${this.route}`)
+    }
+  }
+}
+</script>
