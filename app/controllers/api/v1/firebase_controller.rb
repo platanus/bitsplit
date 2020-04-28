ee3135b446f1303e56fe1f98ca96d3cdfc7f20bd
Daniel Darritchon <dedarritchon@uc.cl>
2020-04-27 20:35:51 -0400

    [backend] add: endpoint updates notification seen status

--- app/controllers/api/v1/firebase_controller.rb
+++ app/controllers/api/v1/firebase_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::FirebaseController < ApplicationController
+
+    acts_as_token_authentication_handler_for User
+
     def update
-        head(:unauthorized)
+        firebase = FirebaseService.new(current_user)
+        firebase.update_notification_status(params[:notification_token])
+        head(:ok)
     end
 
     private
