b677c19e5e243ae07b7d58965034e9d6c9ffcf9a
rihanuch <rihanuch@uc.cl>
2020-04-28 15:15:02 -0400

    [backend] feat: payoff splitwise debt

--- app/controllers/api/v1/splitwise/debts_controller.rb
+++ app/controllers/api/v1/splitwise/debts_controller.rb
@@ -6,9 +6,18 @@ class Api::V1::Splitwise::DebtsController < ApplicationController
         return if !current_user.authenticated_with_splitwise
         @user_groups = @splitwise_service.get_current_user_groups
     end
+
+    def create
+        response = @splitwise_service.payoff_debt(params[:group_id], params[:to_user_id], params[:amount])
+        if response.code == "200"
+          head(:created)
+        else
+          head(:unauthorized)
+        end
+    end
     
     private
     def generate_splitwise_service
       @splitwise_service = SplitwiseService.new(user: current_user)
-    end 
+    end
 end
