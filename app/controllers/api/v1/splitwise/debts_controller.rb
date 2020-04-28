075a584f06b67a37d20ea1bc48fd95d476498256
rihanuch <rihanuch@uc.cl>
2020-04-28 15:14:31 -0400

    [backend] refactor: cleaner show method

--- app/controllers/api/v1/splitwise/debts_controller.rb
+++ app/controllers/api/v1/splitwise/debts_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::Splitwise::DebtsController < ApplicationController
     
+    before_action :generate_splitwise_service
+
     def show 
-        return if current_user.splitwise_secret.nil? || current_user.splitwise_token.nil?
-        splitwise_service = SplitwiseService.new(user: current_user)
-        @user_groups = splitwise_service.get_current_user_groups
+        return if !current_user.authenticated_with_splitwise
+        @user_groups = @splitwise_service.get_current_user_groups
+    end
+    
+    private
+    def generate_splitwise_service
+      @splitwise_service = SplitwiseService.new(user: current_user)
     end 
 end
