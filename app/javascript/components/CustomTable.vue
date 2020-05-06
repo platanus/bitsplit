1f846a52a5dc7ee69648741f0f1e075ca236bc01
Martin Anselmo <mfanselmo@uc.cl>
2020-05-06 01:29:39 -0400

    [web]Design: Fix design for home and landing page

--- app/javascript/components/CustomTable.vue
+++ app/javascript/components/CustomTable.vue
@@ -1,13 +1,19 @@
 <template>
   <table
-    class="table-auto  sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5"
+    class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg "
   >
     <thead class="text-white">
       <slot name="columns">
         <tr
-          class="bg-indigo-500  flex flex-col flex-no wrap sm:table-row rounded-l-lg  mb-2"
+          class="bg-indigo-500 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
+          v-for="(column, index) in data"
+          v-bind:key="index"
         >
-          <th  class="p-3 text-left" v-for="(column, index) in columns" v-bind:key="index">
+          <th
+            class="p-3 text-left"
+            v-for="(column, index) in columns"
+            v-bind:key="index"
+          >
             {{ column }}
           </th>
         </tr>
@@ -15,9 +21,16 @@
     </thead>
     <tbody class="flex-1 sm:flex-none">
       <template v-for="(item, index) in data">
-        <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0" v-bind:key="index">
+        <tr
+          class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0"
+          v-bind:key="index"
+        >
           <slot :row="item">
-            <td class="border-grey-light border hover:bg-gray-100 p-3" v-for="(column, index) in columns" v-bind:key="index">
+            <td
+              class="border-grey-light border hover:bg-gray-100 p-3"
+              v-for="(column, index) in columns"
+              v-bind:key="index"
+            >
               {{ itemValue(item, column) }}
             </td>
           </slot>
--- app/javascript/router/Landing.vue
+++ app/javascript/router/Landing.vue
@@ -2,24 +2,30 @@
   <div class="m-12">
     <div class="bg-gray-200 p-10 my-4 rounded-md">
       <p class="text-5xl font-bold">BitSplit</p>
-      <p
-        class="font-light"
-      >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+      <p class="font-light">
+        Bienvenido a BitSplit, el sitio donde podrás pagar facilmente a tus
+        amigos, cobrar deudas y manejar dinero. Todo esto de forma fácil,
+        intuitiva e internacional!
+      </p>
     </div>
     <div
       class="flex flex-col justify-center md:flex-row md:justify-start bg-gray-200 p-10 rounded-md"
     >
-      <div class="flex flex-col md:flex-row md:items-start text-center px-4 py-2">
+      <div
+        class="flex flex-col md:flex-row md:items-start text-center px-4 py-2"
+      >
         <LinkButton
           classmod="bg-blue-500 hover:bg-blue-700 mx-4 my-3 md:my-0"
           :fieldDisabled="false"
           route="sign-in"
-        >Iniciar sesion</LinkButton>
+          >Iniciar sesion</LinkButton
+        >
         <LinkButton
           classmod="bg-blue-500 hover:bg-blue-700 mx-4"
           :fieldDisabled="false"
           route="sign-up"
-        >Registrarse</LinkButton>
+          >Registrarse</LinkButton
+        >
       </div>
     </div>
   </div>
