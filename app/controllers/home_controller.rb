8784d9081ee4971a06fa18bc89b2fce3ed47bfb9
rihanuch <rihanuch@uc.cl>
2020-05-07 15:26:04 -0400

    [backend] remove: unnecessary double check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
-    #before_action :authenticate_user!, except: :index
-    acts_as_token_authentication_handler_for User, except: [:index]
-
     def index;
     end
 end
