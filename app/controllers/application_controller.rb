944fc037b9de9fad02c4528f0fccef8484175f58
rihanuch <rihanuch@uc.cl>
2020-05-07 15:24:59 -0400

    [backend] fix: force auth on routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
+    # Prevent CSRF attacks, except for JSON requests (API clients)
+    # protect_from_forgery unless: -> { request.format.json? }
 
-    acts_as_token_authentication_handler_for User
+    # Require authentication and do not set a session cookie for JSON requests (API clients)
+    before_action :authenticate_user!, :do_not_set_cookie, if: -> { request.format.json? }
+
+    private
+
+    # Do not generate a session or session ID cookie
+    # See https://github.com/rack/rack/blob/master/lib/rack/session/abstract/id.rb#L171
+    def do_not_set_cookie
+        request.session_options[:skip] = true
+    end
 end
