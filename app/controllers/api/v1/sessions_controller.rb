7e16edf724ee4bf84c9b0f9f7b55f641642039f6
rihanuch <rihanuch@uc.cl>
2020-05-13 18:01:29 -0400

    [backend] fix: update tokens on firebase

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,15 +5,15 @@ class Api::V1::SessionsController < Devise::SessionsController
     def create
       user = warden.authenticate!(auth_options)
       token = Tiddle.create_and_return_token(user, request)
-
-      # Use firebase service to save token
+      # Use firebase service to save newly created token
       FirebaseService.new(user).save_token(token)
-
       render json: { authentication_token: token }
     end
   
     def destroy
       if current_user && Tiddle.expire_token(current_user, request)
+        # Use firebase service to destroy token in header
+        FirebaseService.new(current_user).delete_token(request.headers["X-User-Token"])
         head :ok
       else
         # Client tried to expire an invalid token
