71ce8cc7fb3f461917857556d03dd263732c3e95
Daniel Darritchon <dedarritchon@uc.cl>
2020-05-19 17:39:31 -0400

    [backend] add: small fix

--- app/services/ledgerizer_service.rb
+++ app/services/ledgerizer_service.rb
@@ -1,17 +1,20 @@
 class LedgerizerService < PowerTypes::Service.new
 
     def self.deposit(user, bitcoins)
-        deposit = Deposit.create(user: user, satoshis: to_satoshis(bitcoins))
+        deposit = Deposit.create(user: user,
+            satoshis: to_satoshis(bitcoins))
         RegisterDeposit.for(deposit: deposit)
     end 
 
     def self.withdrawal(user, bitcoins)
-        withdrawal = Withdrawal.create(user: user, satoshis: to_satoshis(bitcoins))
+        withdrawal = Withdrawal.create(user: user,
+            satoshis: to_satoshis(bitcoins))
         RegisterWithdrawal.for(withdrawal: withdrawal)
     end
 
     def self.transfer(sender, receiver, bitcoins)
-        transfer = Transfer.create(sender: sender, receiver: receiver, satoshis: to_satoshis(bitcoins))
+        transfer = Transfer.create(sender: sender,
+            receiver: receiver, satoshis: to_satoshis(bitcoins))
         RegisterTransfer.for(transfer: transfer)
     end
 
