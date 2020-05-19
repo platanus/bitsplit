e2585f2d77ce00cc29424b01521fd8c9f079ec4d
pfarellano <pfarellano@uc.cl>
2020-05-19 16:57:52 -0400

    [backend] format fixes Opennode service

--- app/services/open_node_service.rb
+++ app/services/open_node_service.rb
@@ -1,24 +1,22 @@
 class OpenNodeService < PowerTypes::Service.new(charges_api_key: nil, withdrawals_api_key: nil)
   # Service code goes here
 
-  def send_charge_request (amount, currency)
-    url = 'https://api.opennode.co/v1/charges'
-    headers={ 
-        'Authorization' => @charges_api_key,
-        'Content-Type' => 'application/json'
-          }
+  def send_charge_request(amount, currency)
+    url = url('/v1/charges')
+    headers = headers(@charges_api_key)
     body = { amount: amount, currency: currency}.to_json
     post_request(url, body, headers)
   end
 
-  def send_withdrawal_request (invoice)
-    url ='https://api.opennode.co/v2/withdrawals'
-    headers={ 
-        'Authorization' => @withdrawals_api_key,
-        'Content-Type' => 'application/json'
-          }
+  def send_withdrawal_request(invoice)
+    url = url('/v2/withdrawals')
+    headers = headers(@withdrawals_api_key)
     body = { type: 'ln', address: invoice}.to_json
     post_request(url, body, headers)
+  end
+
+  def url(path)
+    "https://api.opennode.co#{path}"
   end
 
   def post_request(path, body, header)
@@ -27,4 +25,12 @@ class OpenNodeService < PowerTypes::Service.new(charges_api_key: nil, withdrawal
     HTTParty.post(path, body: body, headers: header)
   end
 
+  private
+
+  def headers(api_key)
+    {
+      'Authorization' => api_key,
+      'Content-Type' => 'application/json'
+    }
+  end
 end
