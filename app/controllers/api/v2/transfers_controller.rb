275db8dd3223ffb53cb0b63340f871acfc01cbef
Daniel Darritchon <dedarritchon@uc.cl>
2020-05-20 15:38:56 -0400

    [backend] feat: ledgerizer endpoint

--- app/controllers/api/v2/transfers_controller.rb
+++ app/controllers/api/v2/transfers_controller.rb
@@ -2,7 +2,7 @@ class Api::V2::TransfersController < Api::V2::BaseController
 
   def create
     @receiver = User.find_by(email: params[:receiver_email])
-    if receiver.nil?
+    if @receiver.nil?
       render json: { error: 'invalid receiver email' }, status: 400
     end
     success, @error_message = LedgerizerService.new.transfer(
