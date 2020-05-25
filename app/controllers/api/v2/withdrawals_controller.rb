ba2387fb9df56043b6511457abb7fafcd3fab333
Ignacio Castañeda <icastanedaw@gmail.com>
2020-05-25 15:41:17 -0400

    chore(rubocop): rubocop all files in the codebase

--- app/controllers/api/v2/withdrawals_controller.rb
+++ app/controllers/api/v2/withdrawals_controller.rb
@@ -1,8 +1,8 @@
 class Api::V2::WithdrawalsController < Api::V2::BaseController
-    # class Api::V2::WithdrawalsController < ApplicationController
-    
-    def create
-        request = OpenNodeService.new(withdrawals_api_key: ENV.fetch("OPENNODE_WITHDRAWALS_KEY"))
-        @response = request.send_withdrawal_request(params[:invoice])
-    end
+  # class Api::V2::WithdrawalsController < ApplicationController
+
+  def create
+    request = OpenNodeService.new(withdrawals_api_key: ENV.fetch('OPENNODE_WITHDRAWALS_KEY'))
+    @response = request.send_withdrawal_request(params[:invoice])
+  end
 end
