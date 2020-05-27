831f187c9fbbe96d1513e13a8fd04559d55a1470
rihanuch <rihanuch@uc.cl>
2020-05-27 00:31:55 -0400

    [backend] fix: consistency

--- app/controllers/api/v1/splitwise/debts_controller.rb
+++ app/controllers/api/v1/splitwise/debts_controller.rb
@@ -11,8 +11,7 @@ class Api::V1::Splitwise::DebtsController < ApplicationController
 
   def create
     # respond_with({ error: @error_message }, status: :unprocessable_entity)
-
-    success, @error_message, _new_payment = payment_service.create_payment(create_params)
+    success, @error_message, _new_payment = generate_payment_service.create_payment(payment_params)
     head(:unprocessable_entity) && return unless success
 
     response = splitwise_service.payoff_debt(create_params)
@@ -25,15 +24,19 @@ class Api::V1::Splitwise::DebtsController < ApplicationController
 
   private
 
+  def payment_params
+    params.require(:debt).permit(:group_id, :to_user_id, :payment_amount)
+  end
+
   def create_params
-    params.permit(:group_id, :to_user_id, :amount)
+    params.permit(:group_id, :to_user_id, :payment_amount)
   end
 
   def generate_splitwise_service
     @splitwise_service ||= SplitwiseService.new(user: current_user)
   end
 
-  def payment_service
-    @payments_service ||= PaymentsService.new(sender: current_user, receiver: User.find_by(splitwise_user_id: params[:to_user_id]))
+  def generate_payment_service
+    @payments_service = PaymentsService.new(sender: current_user, receiver: User.find_by(splitwise_user_id: params[:to_user_id]))
   end
 end
