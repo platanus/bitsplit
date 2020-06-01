320c671745f65041143280e6eec1c0e4f00dea15
josefinabengoa <jbengoa1@uc.cl>
2020-06-01 18:19:47 -0400

    [backend] fix: indentation corrections

--- app/commands/convert_satoshis_to_bitcoins.rb
+++ app/commands/convert_satoshis_to_bitcoins.rb
@@ -1,5 +1,5 @@
 class ConvertSatoshisToBitcoins < PowerTypes::Command.new(:satoshis)
-  
+    
     def perform
         @satoshis / 100_000_000
     end
--- app/controllers/api/v2/balances_controller.rb
+++ app/controllers/api/v2/balances_controller.rb
@@ -1,13 +1,13 @@
 class Api::V2::BalancesController < ApplicationController
     
   def show
-      api_key, api_secret = current_user.buda_keys
-      buda_user = BudaUserService.new(api_key: api_key, api_secret: api_secret)
-      unless buda_user.balance('btc').has_key? 'balance'
-        @error_message = balance_btc
-        render('error') && return
-      end
-      @balance_buda_clp, @balance_buda_btc, @balance_bitsplit_btc = helpers.generate_answer(buda_user)
+    api_key, api_secret = current_user.buda_keys
+    buda_user = BudaUserService.new(api_key: api_key, api_secret: api_secret)
+    unless buda_user.balance('btc').has_key? 'balance'
+      @error_message = balance_btc
+      render('error') && return
     end
-    
+    @balance_buda_clp, @balance_buda_btc, @balance_bitsplit_btc = helpers.generate_answer(buda_user)
   end
+    
+end
