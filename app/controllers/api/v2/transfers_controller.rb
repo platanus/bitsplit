5e928c00a9f650e2b6a0a48445bd7cd3d3f94a82
pfarellano <pfarellano@uc.cl>
2020-06-02 23:02:38 -0400

    [backend]transfers controller and endpoint done

--- app/controllers/api/v2/transfers_controller.rb
+++ app/controllers/api/v2/transfers_controller.rb
@@ -1,16 +1,18 @@
 class Api::V2::TransfersController < Api::V2::BaseController
+  
   def create
     receiver = User.find_by(email: params[:receiver_email])
     render(json: { error: 'invalid receiver email' }, status: 400) && return if receiver.nil?
     render(json: { error: 'you can not transfer to yourself' }, status: 400) && return if receiver == current_user
 
     money_service = MoneyService.new(sender: current_user, receiver: receiver, amount: params[:amount], wallet_origin: params[:wallet_origin])
-    success, error_message = money_service.payment
+    success, message = money_service.payment
 
     if success
-      head :ok
+      render json: { payment: message }, status: 200
     else
-      render json: { error: error_message }, status: 400
+      render json: { error: message }, status: 400
     end
   end
+
 end
--- app/services/money_service.rb
+++ app/services/money_service.rb
@@ -14,19 +14,22 @@ class MoneyService < PowerTypes::Service.new(:sender, :receiver, :amount, :walle
       return false, @error_message
     end
 
-    invoice_payment = pay_invoice
-    if invoice_payment == nil
-      return false, @error_message
+    
+    if invoice != false
+      payment = pay_invoice(invoice)
+      if payment == nil
+        return false, @error_message
+      end
+    else
+      payment = "internal payment done"
     end
 
-    make_ledgerazer_registration
-    return true, @error_message
+    make_ledgerizer_registration
+    return true, payment
   end
 
-
   def generate_invoice
-    if receiver_wallet == 'buda'
-      return nil unless check_user_buda_account(@receiver)
+    if @receiver.wallet == 'buda'
       api_key, api_secret = @receiver.buda_keys
       buda_service = BudaUserService.new(api_key: api_key, api_secret: api_secret)
       response = buda_service.generate_invoice(@amount)
@@ -34,39 +37,34 @@ class MoneyService < PowerTypes::Service.new(:sender, :receiver, :amount, :walle
       invoice = JSON.parse(response.body)['invoice']['encoded_payment_request']
     
     else
+      return false unless @wallet_origin != @receiver.wallet
       open_node_service = OpenNodeService.new
       response = open_node_service.send_charge_request(@amount, 'BTC')
       return nil unless check_opennode_response(response)
       invoice = JSON.parse(response.body)['data']['lightning_invoice']['payreq']
     
     end
-    return invoice
+    invoice
   end
 
-  def pay_invoice
+  def pay_invoice(invoice)
     if @wallet_origin == 'buda'
-      return nil unless check_user_buda_account(@sender)
       api_key, api_secret = @sender.buda_keys
       buda_service = BudaUserService.new(api_key: api_key, api_secret: api_secret)
       simulate = ENV.fetch('INVOICE_PAYMENT_SIMULATION')
-      invoice_payment = buda_service.pay_invoice(@amount, invoice, simulate)
-      return nil unless check_buda_invoice_payment(invoice_payment)
+      payment = buda_service.pay_invoice(@amount, invoice, simulate)
+      return nil unless check_buda_invoice_payment(payment)
    
-    else
+    elsif @wallet_origin == 'bitsplit'
       open_node_service = OpenNodeService.new
-      invoice_payment = open_node_service.send_withdrawal_request(invoice)
-      return nil unless check_opennode_response(invoice_payment)
+      payment = open_node_service.send_withdrawal_request(invoice)
+      return nil unless check_opennode_response(payment)
     
+    else
+      @error_message = 'this origin wallet type is not available'
+      return nil
     end
-    return invoice_payment
-  end
-  
-  def check_user_buda_account(user)
-    if user.api_key.nil? || user.api_secret.nil? 
-      @error_message = 'user does not have buda api_keys'
-      return false
-    end 
-    return true
+    payment
   end
 
   def check_buda_invoice_creation(response)
@@ -94,7 +92,7 @@ class MoneyService < PowerTypes::Service.new(:sender, :receiver, :amount, :walle
     return true
   end
 
-  def make_ledgerazer_registration
+  def make_ledgerizer_registration
     @ledgerizer = LedgerizerService.new
     
     if @wallet_origin != 'bitsplit'
