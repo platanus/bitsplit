f45199b79e387e773b4708eece5a9ad8083b3ad9
josefinabengoa <jbengoa1@uc.cl>
2020-06-03 00:05:37 -0400

    [backend] fix: answer in case user do not have buda credentials

--- app/controllers/api/v2/balances_controller.rb
+++ app/controllers/api/v2/balances_controller.rb
@@ -1,11 +1,13 @@
 class Api::V2::BalancesController < ApplicationController
     
   def show
-    api_key, api_secret = current_user.buda_keys
-    buda_user = BudaUserService.new(api_key: api_key, api_secret: api_secret)
-    unless buda_user.balance('btc').has_key? 'balance'
-      @error_message = balance_btc
-      render('error') && return
+    if current_user.api_key
+      api_key, api_secret = current_user.buda_keys
+      buda_user = BudaUserService.new(api_key: api_key, api_secret: api_secret)
+      unless buda_user.balance('btc').has_key? 'balance'
+        @error_message = balance_btc
+        render('error') && return
+      end
     end
     @balance_buda_clp, @balance_buda_btc, @buda_btc_clp, 
     @balance_bitsplit_btc, @balance_bitsplit_btc_clp = helpers.generate_answer(buda_user)
--- app/helpers/api/v2/balances_helper.rb
+++ app/helpers/api/v2/balances_helper.rb
@@ -2,11 +2,11 @@ module Api::V2::BalancesHelper
 
   def generate_answer(buda_user)
     clp_equivalence_btc = ConvertBtcToClp.for(amount: 1).to_f
-    buda_answer_clp = buda_user.balance('clp')
-    buda_answer_btc = buda_user.balance('btc')
-    balance_buda_clp = buda_balance(buda_answer_clp )
+    buda_answer_clp = buda_user&.balance('clp')
+    buda_answer_btc = buda_user&.balance('btc')
+    balance_buda_clp = buda_balance(buda_answer_clp)
     balance_buda_btc = buda_balance(buda_answer_btc)
-    buda_btc_clp = balance_buda_btc_to_clp(balance_buda_btc)
+    buda_btc_clp = balance_buda_btc_to_clp(balance_buda_btc, clp_equivalence_btc)
     balance_bitsplit_btc = bitsplit_balance(1)
     balance_bitsplit_btc_clp = bitsplit_balance(clp_equivalence_btc)
     [balance_buda_clp, balance_buda_btc, buda_btc_clp, 
@@ -15,17 +15,17 @@ module Api::V2::BalancesHelper
 
   def buda_balance(balance_curr)
     {
-      amount: balance_curr['balance']['amount'][0],
-      available_amount: balance_curr['balance']['available_amount'][0],
-      frozen_amount: balance_curr['balance']['frozen_amount'][0],
-      pending_withdraw_amount: balance_curr['balance']['pending_withdraw_amount'][0]
+      amount: balance_curr&['balance']&['amount'][0] || "0.0",
+      available_amount: balance_curr&['balance']&['available_amount'][0] || "0.0",
+      frozen_amount: balance_curr&['balance']&['frozen_amount'][0] || "0.0",
+      pending_withdraw_amount: balance_curr&['balance']&['pending_withdraw_amount'][0] || "0.0"
     }
   end
 
-  def balance_buda_btc_to_clp(balance_buda_btc)
+  def balance_buda_btc_to_clp(balance_buda_btc, clp_equivalence_btc)
     hash = {}
     balance_buda_btc.keys.each do |key|
-      hash[:key] = clp_equivalence_btc * balance_buda_btc[key] 
+      hash[key] = (clp_equivalence_btc * balance_buda_btc[key].to_f).to_s
     end
     hash
   end
