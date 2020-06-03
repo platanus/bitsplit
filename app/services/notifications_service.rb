7349ee1c783ca8ede01d4913eb0410600adaa814
Daniel Darritchon <dedarritchon@uc.cl>
2020-06-02 23:11:15 -0400

    [backend] fix: datetime format

--- app/services/notifications_service.rb
+++ app/services/notifications_service.rb
@@ -1,7 +1,7 @@
 class NotificationsService < PowerTypes::Service.new
 
   def payment_data(sender, amount)
-    data = {sender: sender, amount: amount, created_at: Time.now.to_i}
+    data = {sender: sender, amount: amount, created_at: Time.now}
   end
 
   def payment_notifications(sender, amount)
