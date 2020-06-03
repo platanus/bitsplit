10815204618178a51245e3a04a120e79544eed50
matiaszenteno <mlzenteno@uc.cl>
2020-06-03 18:19:38 -0400

    [web] updated: custom table component

--- app/javascript/components/CustomTable.vue
+++ app/javascript/components/CustomTable.vue
@@ -1,44 +1,20 @@
 <template>
-  <table
-    class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg "
-  >
-    <thead class="text-white">
-      <slot name="columns">
-        <tr
-          class="bg-indigo-500 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
-          v-for="(column, index) in data"
-          :key="index"
-        >
-          <th
-            class="p-3 text-left"
-            v-for="(column, index) in columns"
-            :key="index"
-          >
-            {{ column }}
-          </th>
-        </tr>
-      </slot>
+  <table class="table-auto bg-white">
+    <thead class="bg-indigo-500 text-white">
+      <tr>
+        <th class="p-3" v-for="(column, index) in columns" :key="index">
+          {{ column }}
+        </th>
+      </tr>
     </thead>
-    <tbody class="flex-1 sm:flex-none">
-      <template v-for="(item, index) in data">
-        <tr
-          class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0"
-          :key="index"
-        >
-          <slot :row="item">
-            <td
-              class="border-grey-light border hover:bg-gray-100 p-3"
-              v-for="(column, index) in columns"
-              :key="index"
-            >
-              {{ itemValue(item, column) }}
-            </td>
-          </slot>
-        </tr>
-      </template>
+    <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <slot :row="item" />
+      </tr>
     </tbody>
   </table>
 </template>
+
 <script>
 export default {
   name: 'CustomTable',
@@ -46,10 +22,12 @@ export default {
     columns: Array,
     data: Array,
   },
-  methods: {
-    itemValue(item, column) {
-      return item[column.toLowerCase()];
-    },
+  mounted() {
+    this.$nextTick(() => {
+      this.$el.querySelectorAll('td').forEach(td => {
+        td.classList.add('p-3');
+      });
+    });
   },
 };
 </script>
