60e54c42af748adae9695be08bdac33275722896
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-06-05 11:54:02 -0400

    [web] add: profile design

--- app/javascript/components/profile/ProfileHome.vue
+++ app/javascript/components/profile/ProfileHome.vue
@@ -1,7 +1,44 @@
 // Todo agregar informacion general del usuario
 <template>
   <div>
-    Home del perfil
+    <div class="txt-card lg:flex">
+      <div class="w-1/3 text-center py-8">
+        <div class="border-r">
+          <div class="text-grey-darker mb-2">
+            <span class="text-xl align-top">Bitsplit Wallet</span>
+          </div>
+          <div class="text-sm uppercase text-grey tracking-wide py-10">
+            Balance: 0.0006 BTC
+          </div>
+        </div>
+      </div>
+      <div class="w-1/3 text-center py-8">
+        <div class="border-r">
+          <div class="text-grey-darker mb-2">
+            <span class="text-xl align-top"> Buda Wallet</span>
+          </div>
+          <div class="text-sm uppercase text-grey tracking-wide py-10">
+            0.0006 BTC
+          </div>
+          <div class="text-sm uppercase text-grey tracking-wide">
+            $ 3.000 CLP
+          </div>
+        </div>
+      </div>
+      <div class="w-1/3 text-center py-8">
+        <div>
+          <div class="text-grey-darker mb-2">
+            <span class="text-xl align-top"> Splitwise </span>
+          </div>
+          <div class="text-sm text-grey tracking-wide py-10">
+            Me deben: $ 5.000
+          </div>
+          <div class="text-sm text-grey tracking-wide">
+            Debo a amigos: $ 3.000
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
--- app/javascript/components/profile/ProfileSettings.vue
+++ app/javascript/components/profile/ProfileSettings.vue
@@ -1,11 +1,60 @@
-// Todo
 <template>
   <div>
-    Settings
+    <div class="txt-card h-auto px-48">
+      <div class="flex md:flex-row-reverse flex-wrap">
+        <div class="w-full md:w-3/4 p-4 text-center">
+          <div class="text-left pl-4 pt-8 mb-8">
+            <span class="text-base text-xl mr-2">{{ currentUser.email }}</span>
+          </div>
+          <div class="text-left pl-4 pt-3">
+            <span class="text-base font-semibold mr-2">
+              <b>Wallet actual:</b> {{ currentWallet }}
+            </span>
+
+            <button
+              class="btn ext-black font-bold p-1 rounded focus:outline-none"
+              @click="change()"
+            >
+              Cambiar Wallet
+            </button>
+          </div>
+        </div>
+        <div class="w-full md:w-1/4 p-4 text-center">
+          <div class="w-full relative md:w-3/4 text-center mt-8">
+            <button class="flex">
+              <img
+                src="https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                class="rounded-full border-white border-2 w-34 ml-6 mr-6 mb-3"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
 export default {
   name: 'ProfileSettings',
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions('user', ['changeWallet']),
+    change() {
+      this.changeWallet(this.currentWallet);
+    },
+  },
+  computed: {
+    ...mapState('user', [
+      'currentUser',
+      'userBalanceCLP',
+      'userBalanceBTC',
+      'currentWallet',
+    ]),
+    ...mapGetters('user', ['budaSignedIn', 'splitwiseSignedIn']),
+  },
 };
 </script>
