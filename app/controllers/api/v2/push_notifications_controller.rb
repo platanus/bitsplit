dbf8ab44baadbca0d9d29a6cfd2e306b0ec490b6
Daniel Darritchon <dedarritchon@uc.cl>
2020-06-17 16:42:23 -0400

    [backend] fix: Code review comments

--- app/controllers/api/v2/push_notifications_controller.rb
+++ app/controllers/api/v2/push_notifications_controller.rb
@@ -6,12 +6,10 @@ class Api::V2::PushNotificationsController < Api::V2::BaseController
     if token.nil?
       NotificationToken.create(:token => params[:token], :user => current_user, :authentication_token => auth_token)
       render json: { message: "push notification token saved" }, status: 200 and return
-    else
-      if token.user != current_user
-        token.update(:user => current_user)
-        token.update(:authentication_token => auth_token)
-        render json: { message: "push notification token updated" }, status: 200 and return
-      end
+    elsif token.user != current_user
+      token.update({:user => current_user,
+        :authentication_token => auth_token})
+      render json: { message: "push notification token updated" }, status: 200 and return
     end
     render json: { message: "token already exists" }, status: 409 and return
   end
