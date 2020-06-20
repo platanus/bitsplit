474628e51bda9dc6f6ebdc8ce2e5f6fe046aca0d
pfarellano <pfarellano@uc.cl>
2020-06-20 13:31:56 -0400

    [backend] fix credential validations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,15 +6,7 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def create
     new_user = User.create!(user_params)
-    if user_params.has_key? 'api_key'
-      api_key, api_secret = new_user.buda_keys
-      buda_user = BudaUserService.new(user: new_user, api_key: api_key, api_secret: api_secret)
-      success, message = buda_user.validate_keys
-      if !success
-        render(json: { user: "created", buda_credentials_error: message }) && return
-      end
-    end
-    render(json: {data: Api::V1::UserSerializer.new(new_user)})  && return
+    respond_with new_user
   end
 
   def show
@@ -25,14 +17,14 @@ class Api::V1::UsersController < Api::V1::BaseController
     if (user_params.has_key?(:api_key) || user_params.has_key?(:api_secret))
       if !current_user&.valid_password?(params[:password])
         # user does not have the correct password => cannot update correctly
-        render(json: {error: "incorrect password" }, status: 400) && return
+        respond_with({error:"incorrect password" }, :status => 400) && return 
       else
         buda_user = BudaUserService.new(user: current_user, api_key: params[:api_key], api_secret: params[:api_secret])
         success, message = buda_user.validate_keys
-        render(json: {buda_credentials_error: message }) && return unless success
+        respond_with({error: message}, :status => 400) && return unless success
       end
     current_user.update(user_params)
-    render(json: {data: Api::V1::UserSerializer.new(current_user)}) && return
+    respond_with current_user
     end
   end
 
