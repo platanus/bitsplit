34ff1f2406e1ebc2dcbe47467a100e70204ae119
Daniel Darritchon <dedarritchon@uc.cl>
2020-06-26 22:39:47 -0400

    [backend] add: endpoint tested

--- app/controllers/api/v2/buda_withdrawals_controller.rb
+++ app/controllers/api/v2/buda_withdrawals_controller.rb
@@ -4,18 +4,21 @@ class Api::V2::BudaWithdrawalsController < ApplicationController
     if current_user.api_key && !decrypt(current_user.api_key).empty?
       api_key, api_secret = current_user.buda_keys
       buda_service = BudaUserService.new(api_key: api_key, api_secret: api_secret)
-      @invoice = buda_service.generate_invoice(params[:amount])
+      response = buda_service.generate_invoice(params[:amount])
+      invoice = JSON.parse(response.body)['invoice']['encoded_payment_request']
       satoshis_amount = satoshi_price(params[:amount])
       money_service = MoneyService.new
+      render(json: { error: 'invalid buda invoice' }, status: 400) and return unless money_service.check_buda_invoice_creation(response)
       amount_validation, message = money_service.validate_amount(current_user, satoshis_amount)
-      @response = message
-      return unless amount_validation
+      render(json: { error: message }, status: 400) and return unless amount_validation
       new_user_withdrawal = UserWithdrawal.create!(user_id: current_user.id,
                                             amount: params[:amount], 
                                             completed: false,
-                                            invoice: @invoice )
-      response = opennode_service.send_withdrawal_request(@invoice, false)
-      @response = JSON.parse(response.body)
+                                            invoice: invoice )
+      opennode_service = OpenNodeService.new
+      response = opennode_service.send_withdrawal_request(invoice, false)
+      render(json: { error: 'invalid opennode withdrawal request' }, status: 400) and return unless money_service.check_opennode_response(response)
+      render(json: JSON.parse(response.body), status: 200) and return
     else
       render(json: { error: 'user does not have synchronized Buda' }, status: 400) 
     end
