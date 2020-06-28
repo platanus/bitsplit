e18eb40e5eba2015441f2371fd7890d323a4fcd0
pfarellano <pfarellano@uc.cl>
2020-06-27 21:05:24 -0400

    [backend] fix user patch

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -24,6 +24,7 @@ class Api::V1::UsersController < Api::V1::BaseController
         respond_with({error: message}, :status => 400) && return unless success
       end
     end
+  
     current_user.update(user_params)
     respond_with current_user
   end
