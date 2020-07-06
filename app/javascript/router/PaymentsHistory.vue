59a4d336ac2ef561e58456d297bb09aa6609441e
TOMAS NAVARRETE <tanavarrete@uc.cl>
2020-07-05 21:20:48 -0400

    fix: remove user internal transfers

--- app/javascript/router/PaymentsHistory.vue
+++ app/javascript/router/PaymentsHistory.vue
@@ -18,41 +18,48 @@
             <div v-if="userPaymentsHistory.length">
               <CustomTable :data="userPaymentsHistory" :columns="tableColumns">
                 <template #default="{ row }">
-                  <td
-                    class="border-grey-light border hover:bg-gray-100 p-3"
-                    v-if="row.attributes.sender.email != currentUser.email"
+                  <div
+                    v-if="
+                      row.attributes.sender.email !=
+                      row.attributes.receiver.email
+                    "
                   >
-                    <span
-                      class="px-2 items-center inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                    <td
+                      class="border-grey-light border hover:bg-gray-100 p-3"
+                      v-if="row.attributes.sender.email != currentUser.email"
                     >
-                      Recibido
-                    </span>
-                  </td>
-                  <td
-                    v-else
-                    class="border-grey-light border hover:bg-gray-100 p-3"
-                  >
-                    <span
-                      class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800"
+                      <span
+                        class="px-2 items-center inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                      >
+                        Recibido
+                      </span>
+                    </td>
+                    <td
+                      v-else
+                      class="border-grey-light border hover:bg-gray-100 p-3"
                     >
-                      Enviado
-                    </span>
-                  </td>
-                  <td class="border-grey-light border hover:bg-gray-100 p-3">
-                    {{ row.attributes.sender.email }}
-                  </td>
-                  <td class="border-grey-light border hover:bg-gray-100 p-3">
-                    {{ row.attributes.receiver.email }}
-                  </td>
-                  <td class="border-grey-light border hover:bg-gray-100 p-3">
-                    {{ row.attributes.amount_btc }} BTC
-                  </td>
-                  <td class="border-grey-light border hover:bg-gray-100 p-3">
-                    $ {{ row.attributes.amount_clp.toFixed(0) }} CLP
-                  </td>
-                  <td class="border-grey-light border hover:bg-gray-100 p-3">
-                    {{ getDate(row.attributes.created_at) }}
-                  </td>
+                      <span
+                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800"
+                      >
+                        Enviado
+                      </span>
+                    </td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">
+                      {{ row.attributes.sender.email }}
+                    </td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">
+                      {{ row.attributes.receiver.email }}
+                    </td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">
+                      {{ row.attributes.amount_btc }} BTC
+                    </td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">
+                      $ {{ row.attributes.amount_clp.toFixed(0) }} CLP
+                    </td>
+                    <td class="border-grey-light border hover:bg-gray-100 p-3">
+                      {{ getDate(row.attributes.created_at) }}
+                    </td>
+                  </div>
                 </template>
               </CustomTable>
             </div>
