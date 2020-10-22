21df7876e3ac3e7d92c5df3fc114481362f660e2
Raimundo Herrera <rjherrera@uc.cl>
2020-10-22 12:42:11 -0300

    fix(auth): protect from forgery only for non api requests using ActionController::API is not possible with AA, so ActionController::Base had to be used, and this implied CSRF would be enforced again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks, except for JSON requests (API clients)
-  # protect_from_forgery unless: -> { request.format.json? }
+  protect_from_forgery unless: -> { request.format.json? }
 
   # Require authentication and do not set a session cookie for JSON requests (API clients)
   before_action :authenticate_user!, :skip_session, if: -> { request.format.json? }
